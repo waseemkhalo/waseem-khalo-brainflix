35d1dad5e6b380f44d9aea37bcab95e12aab31e9
Waseem <waseemkhalo@gmail.com>
2022-11-04 00:21:35 -0400

    add global scss

--- src/component/Header.js
+++ src/component/Header.js
@@ -8,23 +8,23 @@ function Header() {
     return (
         <header className="header">
 
-            <div className="logo">
+            <div className="header__container">
                 <img className="logo__image" src={logo} alt="brainflix logo" ></img>
-                <h1 className="logo__title">Brainflix</h1>
+
+                <div className="form__container" >
+                    <form className="form">
+                        <input className="form__search">
+                        </input>
+                        <button className="form__upload-button">
+                            <img className="form__upload-icon" alt="Upload">
+                            </img>
+                        </button>
+                    </form>
+                    <img className="form__profile-picture" src={pfp} alt="profile">
+                    </img>
+                </div>
             </div>
 
-            <div className="form__container" >
-                <form className="form">
-                    <input className="form__search">
-                    </input>
-                    <button className="form__upload-button">
-                        <img className="form__upload-icon" alt="Upload">
-                        </img>
-                    </button>
-                </form>
-                <img className="form__profile-picture" src={pfp} alt="profile">
-                </img>
-            </div>
         </header>
     );
 }
