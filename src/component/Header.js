9c7af9bf26a64c2e632ca0705f229e38acffe9ba
Waseem <waseemkhalo@gmail.com>
2022-11-04 01:13:40 -0400

    add basic navbar styling

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,7 @@
 import '../styles/Header.scss'
 import pfp from '../assets/images/Mohan-muruge.jpg'
 import logo from '../assets/Logo/BrainFlix-logo.svg'
+import searchIcon from '../assets/Icons/search.svg'
 
 function Header() {
 
@@ -9,20 +10,27 @@ function Header() {
         <header className="header">
 
             <div className="header__container">
-                <img className="logo__image" src={logo} alt="brainflix logo" ></img>
+                <img className="logo" src={logo} alt="brainflix logo" ></img>
 
                 <div className="form__container" >
                     <form className="form">
-                        <input className="form__search">
-                        </input>
-                        <button className="form__upload-button">
-                            <img className="form__upload-icon" alt="Upload">
+
+                        <div className='form__search-container'>
+                            <input src={searchIcon} className="form__search" placeholder='Search'></input>
+                            <img
+                                className="form__profile-picture"
+                                src={pfp}
+                                alt="profile">
                             </img>
+                        </div>
+
+                        <button className="form__upload-button">
+                            Upload
                         </button>
+
                     </form>
-                    <img className="form__profile-picture" src={pfp} alt="profile">
-                    </img>
                 </div>
+                
             </div>
 
         </header>
