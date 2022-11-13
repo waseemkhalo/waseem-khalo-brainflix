fc52f22f049df926070aa08d0bf9453734e52856
Waseem <waseemkhalo@gmail.com>
2022-11-13 18:34:19 -0500

    add tablet breakpoint

--- src/component/UploadPageForm/UploadPageForm.js
+++ src/component/UploadPageForm/UploadPageForm.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom';
 function UploadPageForm() {
 
     return (
-        <div className='upload__form'>
-            <form >
+        <div className='upload__form-container'>
+            <form className='upload__form'>
                 <label className='upload__page-subheader'>Title your video</label>
                 <input className='upload__video-title' src={''} placeholder='Add a title to your video'></input>
                 <label className='upload__page-subheader' htmlFor='comment'>Add video description</label>
                 <textarea className='upload__description-box' placeholder="Add a description to your video"></textarea>
-                <button className='upload__form-button'>Publish</button>
-                <Link to={'/'}> <button className='upload__form-cancel'>Cancel</button> </Link>
+                <div className='upload__button-container'>
+                    <button className='upload__form-button'>Publish</button>
+                    <Link to={'/'}> <button className='upload__form-cancel'>Cancel</button> </Link>
+                </div>
             </form>
         </div>
     )
