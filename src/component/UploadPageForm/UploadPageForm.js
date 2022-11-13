0fd19e928461459fc3653a14c94157af5247cf4e
Waseem <waseemkhalo@gmail.com>
2022-11-13 18:12:42 -0500

    add mobile styling

--- src/component/UploadPageForm/UploadPageForm.js
+++ src/component/UploadPageForm/UploadPageForm.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom';
 function UploadPageForm() {
 
     return (
-        <form>
-            <label>Title your video</label>
-            <input src={''} placeholder='Add a title to your video'></input>
-            <label htmlFor='comment' className='comment__title'>Add video description</label>
-            <textarea className='comment__box' placeholder="Add a description to your video"></textarea>
-            <button>Publish</button>
-            <Link to={'/'}> <button>Cancel</button> </Link>
-        </form>
+        <div className='upload__form'>
+            <form >
+                <label className='upload__page-subheader'>Title your video</label>
+                <input className='upload__video-title' src={''} placeholder='Add a title to your video'></input>
+                <label className='upload__page-subheader' htmlFor='comment'>Add video description</label>
+                <textarea className='upload__description-box' placeholder="Add a description to your video"></textarea>
+                <button className='upload__form-button'>Publish</button>
+                <Link to={'/'}> <button className='upload__form-cancel'>Cancel</button> </Link>
+            </form>
+        </div>
     )
 }
 
--- src/component/UploadPageMain/UploadPageMain.js
+++ src/component/UploadPageMain/UploadPageMain.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import uploadedVideoTemp from '../../assets/images/Upload-video-preview.jpg'
 import UploadPageForm from '../UploadPageForm/UploadPageForm';
+import './UploadPageMain.scss'
 
 function UploadPageMain() {
     return (
-        <div>
-            <h1>Upload Video</h1>
+        <div className='upload'>
+            <h1 className='upload__page-header'>Upload Video</h1>
             <div>
-                <h2>Video Thumbnail</h2>
-                <img src={uploadedVideoTemp}></img>
+                <h2 className='upload__page-subheader'>Video Thumbnail</h2>
+                <img className= 'upload__picture' src={uploadedVideoTemp}></img>
             </div>
 
-            <div>
+            <div className='upload__form'>
                 <UploadPageForm />
             </div>
         </div>
